5c276163610df02b88dfa5c918810156f5b83af4
Alicia Qiu Cheung <aliciaqiu27@gmail.com>
2021-02-08 14:18:21 -0700

    initial commit

--- server.js
+++ server.js
@@ -1,42 +1,22 @@
 // Dependencies
 // =============================================================
+var bodyParser = require("body-parser");
 var express = require("express");
-var path = require("path");
+var path = require ('path')
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-// Star Wars Characters (DATA)
-// =============================================================
-var characters = [
-  {
-    routeName: "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
-  },
-  {
-    routeName: "darthmaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
-  },
-  {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Master",
-    age: 55,
-    forcePoints: 1350
-  }
-];
+require('./routing/api-routes.js')(app);
+require('./routing/html-routes.js')(app);
+
 
 // Routes
 // =============================================================
