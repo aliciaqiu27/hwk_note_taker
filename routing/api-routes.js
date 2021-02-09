80f81c5726637491de24c8c8f8207f39fa7ba695
Alicia Qiu Cheung <aliciaqiu27@gmail.com>
2021-02-08 17:02:17 -0700

    added routes and GET/POST.

---
+++ routing/api-routes.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const fs = require("fs")
+const notesArray = require("../db/db.json");
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = app => {
+    app.get("/api/notes", (req, res) => {
+        res.json(notesArray);
+    })
+ 
+    app.post("/api/notes", (req, res) => {
+        const newNote = req.body;
+       const file = path.join(__dirname, "../db/db.json");
+
+        newNote.id = uuidv4();
+        notesArray.push(newNote);
+
+       fs.writeFile(file, JSON.stringify(notesArray, null, 4), err => {
+           if(err) throw err;
+           console.log("Note saved!");
+       });
+        res.send(newNote)
+});
+
+    app.delete("/api/notes/:id", (req,res) => {
+        const id = req.params.id;
+        const file = path.join(__dirname, "..db/db.json");
+
+        for(const note of notesArray) {
+            if(id === note.id) {
+                const index = notesArray.indexOf(note);
+                notesArray.splice(index, 1);
+                fs.writeFile(file, JSON.stringify(notesArray, null, 4), err => {
+                    if(err) throw err;
+                    console.log("Note deleted.");
+                });
+                res.end();
+            }
+        }
+    })
+
+
+
+
+
+}
---
+++ routing/html-routes.js
@@ -0,0 +1,10 @@
+const { UV_FS_O_FILEMAP } = require('constants')
+var path = require('path');
+module.exports = function(app) {
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname + 'index.html'))
+    });
+    app.get('/notes', function(req, res) {
+        res.sendFile(path.join(__dirname + 'notes.html'))
+    });
+}
